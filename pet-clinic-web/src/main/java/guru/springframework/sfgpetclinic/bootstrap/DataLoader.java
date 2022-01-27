108e664966e96bbd8d831b15009b135e964b8784
Romanciuc Adrian <romanciucadrian1@gmail.com>
2022-01-27 21:24:58 +0200

    Nu mai tinem seama de id-ul unui obiect

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,14 +23,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Romanciuc");
         owner1.setLastName("Adrian");
 
         ownerService.save(owner1);
-
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rares");
         owner2.setLastName("Vladescu");
 
@@ -39,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
